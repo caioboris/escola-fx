fe4539b0f5d091c9f850160ec6869df16f959df0
Caio Boris <caio.correa@neomode.com.br>
2023-08-31 20:55:06 -0300

    aula-31/08/2023

---
+++ src/main/java/com/example/controller/PrimaryController.java
@@ -0,0 +1,93 @@
+package com.example.controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.example.data.AlunoDao;
+import com.example.model.Aluno;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+
+    @FXML
+    TextField txtNome;
+    @FXML
+    TextField txtTurma;
+    @FXML
+    TextField txtRm;
+    @FXML
+    ListView<Aluno> lista;
+    @FXML
+    RadioButton optOrdenarPorNome;
+    @FXML
+    RadioButton optOrdenarPorTurma;
+
+    ArrayList<Aluno> alunos = new ArrayList<>();
+    AlunoDao dao = new AlunoDao();
+
+    public void adicionarAluno() {
+        var aluno = carregarAlunoDoFormulario();
+
+        try {
+            dao.inserir(aluno);
+        } catch (SQLException e) {
+            mostrarMensagem(AlertType.ERROR, "Erro", e.getMessage());
+        }
+
+        limparFormulario();
+
+        atualizarTela();
+    }
+
+    private void limparFormulario() {
+        txtNome.clear();
+        txtTurma.clear();
+        txtRm.clear();
+    }
+
+    private Alert mostrarMensagem(AlertType tipo, String titulo, String mensagem) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setContentText(mensagem);
+        alert.show();
+
+        return alert;
+    }
+
+    private Aluno carregarAlunoDoFormulario() {
+        return new Aluno(
+                txtNome.getText(),
+                txtTurma.getText(),
+                Integer.valueOf(txtRm.getText()));
+    }
+
+    public void atualizarTela(){
+        lista.getItems().clear();
+        
+        try {
+            dao.buscarTodos().forEach(aluno -> lista.getItems().add(aluno));
+           
+        } catch (SQLException e) {
+
+            mostrarMensagem(AlertType.ERROR, "ERRO", e.getMessage());
+        }
+        
+    }
+
+    public void apagar(){
+        var aluno = lista.getSelectionModel().getSelectedItem();
+        try {
+            dao.apagar(aluno);
+            atualizarTela();
+        } catch (SQLException e) {
+            mostrarMensagem(AlertType.ERROR, "Erro", e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/com/example/model/Aluno.java
@@ -0,0 +1,10 @@
+package com.example.model;
+
+public record Aluno (String nome, String turma, Integer rm) {
+
+    @Override
+    public String toString(){
+        return rm + ": " + nome + " - " + turma;
+    }
+
+}
